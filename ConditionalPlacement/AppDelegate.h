34cf7e7b7fc03ac525fd0ebfb7b73591216a6195
Dustin Flanary <singflan@gmail.com>
2015-01-06 09:55:36 -0700

    Initial Commit

---
+++ ConditionalPlacement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConditionalPlacement
+//
+//  Created by Julieta Flanary on 1/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConditionalPlacement/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ConditionalPlacement
+//
+//  Created by Julieta Flanary on 1/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
